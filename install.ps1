35a1d060203749b6dfe6799926aad076d8eb58c0
Gary Oberbrunner <garyo@darkstarsystems.com>
2018-12-11 22:35:55 -0500

    Add powershell script to install symlinks on Windows

---
+++ install.ps1
@@ -0,0 +1,29 @@
+# Powershell script to install symlinks to dotfiles
+# Before running this script, run this in Powershell (as Admin):
+#  Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+# Then run the script:
+#  install.ps1
+
+# Elevate if not already admin:
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+# Files to symlink
+$files = @("emacs", "bashrc", "emacs-orgmode", "hgrc", "gitconfig",
+           "profile", "pythonstartup", "vimrc", "zshrc")
+$dirs = @("..", "c:/msys64/home/garyo")
+foreach ($d in $dirs) {
+  echo "Installing into '$d'..."
+  if (Test-Path $d) {
+    foreach ($f in $files) {
+      if (-not (Test-Path $d/.$f)) {
+        New-Item -type SymbolicLink -path $d/.$f -value $f
+      }
+    }
+  }
+}
